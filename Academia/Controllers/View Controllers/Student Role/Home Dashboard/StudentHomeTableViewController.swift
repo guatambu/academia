d5f0e24f49c6c63ccce1a25d2f9793d78d6c1a64
guatambu <guatambu@dundak.com>
2018-09-22 21:12:01 -0700

    setup up student dashboard cells for proper display and linked up belt system TVCs

--- Academia/Controllers/View Controllers/Student Role/Home Dashboard/StudentHomeTableViewController.swift
+++ Academia/Controllers/View Controllers/Student Role/Home Dashboard/StudentHomeTableViewController.swift
@@ -12,7 +12,7 @@ class StudentHomeTableViewController: UITableViewController {
     
     // MARK: - Properties
     
-    var adultStudent: AdultStudent?
+    var adultStudent = MockData.adultA
     var kidStudent: KidStudent?
     
     
@@ -23,6 +23,8 @@ class StudentHomeTableViewController: UITableViewController {
                            NSAttributedStringKey.font: UIFont(name: "Avenir-Medium", size: 24)! ]
         
         navigationController?.navigationBar.titleTextAttributes = avenirFont
+        
+        tableView.estimatedRowHeight = 80
     }
     
     
@@ -70,84 +72,59 @@ class StudentHomeTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-//        if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "startGettingPaid" {
-//            
-//            // instantiate the relevant storyboard
-//            let ownerProfileFlowView: UIStoryboard = UIStoryboard(name: "OwnerProfileFlow", bundle: nil)
-//            // instantiate the desired TableViewController as ViewController on relevant storyboard
-//            let destViewController = ownerProfileFlowView.instantiateViewController(withIdentifier: "toOwnerPaymentInfo") as! OwnerPaymentInfoTableViewController
-//            // create the segue programmatically
-//            self.navigationController?.pushViewController(destViewController, animated: true)
-//            // set the desired properties of the destinationVC's navgation Item
-//            let backButtonItem = UIBarButtonItem()
-//            backButtonItem.title = " "
-//            navigationItem.backBarButtonItem = backButtonItem
-//            
-//        } else if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "setUpPaymentPrograms" {
-//            
-//            // instantiate the relevant storyboard
-//            let ownerProfileFlowView: UIStoryboard = UIStoryboard(name: "OwnerProfileFlow", bundle: nil)
-//            // instantiate the desired TableViewController as ViewController on relevant storyboard
-//            let destViewController = ownerProfileFlowView.instantiateViewController(withIdentifier: "toOwnerPaymentPrograms") as! OwnerPaymentProgramsTableViewController
-//            // create the segue programmatically
-//            self.navigationController?.pushViewController(destViewController, animated: true)
-//            // set the desired properties of the destinationVC's navgation Item
-//            let backButtonItem = UIBarButtonItem()
-//            backButtonItem.title = " "
-//            navigationItem.backBarButtonItem = backButtonItem
-//            
-//        } else if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "locationsSetUp" {
-//            
-//            // instantiate the relevant storyboard
-//            let ownerProfileFlowView: UIStoryboard = UIStoryboard(name: "OwnerProfileFlow", bundle: nil)
-//            // instantiate the desired TableViewController as ViewController on relevant storyboard
-//            let destViewController = ownerProfileFlowView.instantiateViewController(withIdentifier: "toOwnerLocationsList") as! MyLocationsTableViewController
-//            // create the segue programmatically
-//            self.navigationController?.pushViewController(destViewController, animated: true)
-//            // set the desired properties of the destinationVC's navgation Item
-//            let backButtonItem = UIBarButtonItem()
-//            backButtonItem.title = " "
-//            navigationItem.backBarButtonItem = backButtonItem
-//            
-//        } else if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "messagingGroups" {
-//            
-//            // instantiate the relevant storyboard
-//            let ownerStudentsFlowView: UIStoryboard = UIStoryboard(name: "OwnerStudentsFlow", bundle: nil)
-//            // instantiate the desired TableViewController as ViewController on relevant storyboard
-//            let destViewController = ownerStudentsFlowView.instantiateViewController(withIdentifier: "toOwnerGroups") as! StudentsByListGroupingTableViewController
-//            // create the segue programmatically
-//            self.navigationController?.pushViewController(destViewController, animated: true)
-//            // set the desired properties of the destinationVC's navgation Item
-//            let backButtonItem = UIBarButtonItem()
-//            backButtonItem.title = " "
-//            navigationItem.backBarButtonItem = backButtonItem
-//            
-//        } else if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "createClassSchedule" {
-//            
-//            // instantiate the relevant storyboard
-//            let ownerBaseCampFlowView: UIStoryboard = UIStoryboard(name: "OwnerBaseCampFlow", bundle: nil)
-//            // instantiate the desired TableViewController as ViewController on relevant storyboard
-//            let destViewController = ownerBaseCampFlowView.instantiateViewController(withIdentifier: "toOwnerClasses") as! OwnerClassesTableViewController
-//            // create the segue programmatically
-//            self.navigationController?.pushViewController(destViewController, animated: true)
-//            // set the desired properties of the destinationVC's navgation Item
-//            let backButtonItem = UIBarButtonItem()
-//            backButtonItem.title = " "
-//            navigationItem.backBarButtonItem = backButtonItem
-//            
-//        } else if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "reviewBeltSystems" {
-//            
-//            // instantiate the relevant storyboard
-//            let ownerBeltSystemFlowView: UIStoryboard = UIStoryboard(name: "OwnerBeltSystemFlow", bundle: nil)
-//            // instantiate the desired TableViewController as ViewController on relevant storyboard
-//            let destViewController = ownerBeltSystemFlowView.instantiateViewController(withIdentifier: "UITableViewController-TZb-1N-cMc") as! BeltSystemsTableViewController
-//            // create the segue programmatically
-//            self.navigationController?.pushViewController(destViewController, animated: true)
-//            // set the desired properties of the destinationVC's navgation Item
-//            let backButtonItem = UIBarButtonItem()
-//            backButtonItem.title = " "
-//            navigationItem.backBarButtonItem = backButtonItem
-//        }
+        
+        if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "setUpPaymentPrograms" {
+            
+            // instantiate the relevant storyboard
+            let ownerProfileFlowView: UIStoryboard = UIStoryboard(name: "StudentBaseCampFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerProfileFlowView.instantiateViewController(withIdentifier: "toOwnerPaymentPrograms") as! OwnerPaymentProgramsTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+            
+        } else if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "messagingGroups" {
+            
+            // instantiate the relevant storyboard
+            let ownerStudentsFlowView: UIStoryboard = UIStoryboard(name: "StudentBaseCampFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerStudentsFlowView.instantiateViewController(withIdentifier: "toOwnerGroups") as! StudentsByListGroupingTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+            
+        } else if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "viewClassSchedule" {
+            
+            // instantiate the relevant storyboard
+            let ownerBaseCampFlowView: UIStoryboard = UIStoryboard(name: "StudentBaseCampFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerBaseCampFlowView.instantiateViewController(withIdentifier: "toOwnerClasses") as! OwnerClassesTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+            
+        } else if AdultStudentModelController.shared.adultStudentOnboardingTasks[indexPath.item].name == "learnTheBeltSystems" {
+            
+            // instantiate the relevant storyboard
+            let ownerBeltSystemFlowView: UIStoryboard = UIStoryboard(name: "OwnerBeltSystemFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerBeltSystemFlowView.instantiateViewController(withIdentifier: "UITableViewController-TZb-1N-cMc") as! BeltSystemsTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+        }
     }
     
     // Override to support conditional editing of the table view.
