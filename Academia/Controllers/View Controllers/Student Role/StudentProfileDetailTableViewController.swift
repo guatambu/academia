8ce351c242b5b5546b31a9d335fa843d5c8f2fce
guatambu <guatambu@dundak.com>
2018-09-26 12:40:27 -0700

    wired up cell segue buttons in student profile static cells in StudentProfileDetailTVC

--- Academia/Controllers/View Controllers/Student Role/StudentProfileDetailTableViewController.swift
+++ Academia/Controllers/View Controllers/Student Role/StudentProfileDetailTableViewController.swift
@@ -9,87 +9,139 @@
 import UIKit
 
 class StudentProfileDetailTableViewController: UITableViewController {
+    
+    //MARK: - Properties
+    
+    @IBOutlet weak var studentProfileMenuCell: UITableViewCell!
+    @IBOutlet weak var beltSystemsMenuCell: UITableViewCell!
+    @IBOutlet weak var studentPaymentDetailsMenuCell: UITableViewCell!
+    @IBOutlet weak var socialNetworksMenuCell: UITableViewCell!
+    @IBOutlet weak var tutorialsMenuCell: UITableViewCell!
+    @IBOutlet weak var privacyInfoMenuCell: UITableViewCell!
+    @IBOutlet weak var aboutAcademiaMenuCell: UITableViewCell!
+    @IBOutlet weak var aboutAcademiaServicesMenuCell: UITableViewCell!
+    
+    
+    //MARK: - ViewController Lifecycle Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //MARK: - Actions
+    
+    @IBAction func myProfileButtonTapped(_ sender: UIButton) {
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    @IBAction func beltSystemsButtonTapped(_ sender: UIButton) {
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    @IBAction func studentPaymentDetailsButtonTapped(_ sender: UIButton) {
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    @IBAction func socialNetworksButtonTapped(_ sender: UIButton) {
     }
-    */
-
-    /*
+    @IBAction func tutorialsButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func privacyInfoButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func aboutAcademiaButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func aboutAcademiaServicesButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    //MARK: - TabelView Functions
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView {
+            
+            // instantiate the relevant storyboard
+            let ownerProfileFlowView: UIStoryboard = UIStoryboard(name: "OwnerProfileFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerProfileFlowView.instantiateViewController(withIdentifier: "toOwnerPaymentInfo") as! OwnerPaymentInfoTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+            
+        } else if OwnerModelController.shared.ownerOnboardingTasks[indexPath.item].name == "setUpPaymentPrograms" {
+            
+            // instantiate the relevant storyboard
+            let ownerProfileFlowView: UIStoryboard = UIStoryboard(name: "OwnerProfileFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerProfileFlowView.instantiateViewController(withIdentifier: "toOwnerPaymentPrograms") as! OwnerPaymentProgramsTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+            
+        } else if OwnerModelController.shared.ownerOnboardingTasks[indexPath.item].name == "locationsSetUp" {
+            
+            // instantiate the relevant storyboard
+            let ownerProfileFlowView: UIStoryboard = UIStoryboard(name: "OwnerProfileFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerProfileFlowView.instantiateViewController(withIdentifier: "toOwnerLocationsList") as! MyLocationsTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+            
+        } else if OwnerModelController.shared.ownerOnboardingTasks[indexPath.item].name == "messagingGroups" {
+            
+            // instantiate the relevant storyboard
+            let ownerStudentsFlowView: UIStoryboard = UIStoryboard(name: "OwnerStudentsFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerStudentsFlowView.instantiateViewController(withIdentifier: "toOwnerGroups") as! StudentsByListGroupingTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+            
+        } else if OwnerModelController.shared.ownerOnboardingTasks[indexPath.item].name == "createClassSchedule" {
+            
+            // instantiate the relevant storyboard
+            let ownerBaseCampFlowView: UIStoryboard = UIStoryboard(name: "OwnerBaseCampFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerBaseCampFlowView.instantiateViewController(withIdentifier: "toOwnerClasses") as! OwnerClassesTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+            
+        } else if OwnerModelController.shared.ownerOnboardingTasks[indexPath.item].name == "reviewBeltSystems" {
+            
+            // instantiate the relevant storyboard
+            let ownerBeltSystemFlowView: UIStoryboard = UIStoryboard(name: "OwnerBeltSystemFlow", bundle: nil)
+            // instantiate the desired TableViewController as ViewController on relevant storyboard
+            let destViewController = ownerBeltSystemFlowView.instantiateViewController(withIdentifier: "UITableViewController-TZb-1N-cMc") as! BeltSystemsTableViewController
+            // create the segue programmatically
+            self.navigationController?.pushViewController(destViewController, animated: true)
+            // set the desired properties of the destinationVC's navgation Item
+            let backButtonItem = UIBarButtonItem()
+            backButtonItem.title = " "
+            navigationItem.backBarButtonItem = backButtonItem
+        }
+    }
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
