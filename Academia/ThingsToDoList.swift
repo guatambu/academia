23376ce04cb2e361b7eccc3ab2bc94f70a55131b
guatambu <guatambu@dundak.com>
2018-10-03 14:30:48 -0700

    added to do list

---
+++ Academia/ThingsToDoList.swift
@@ -0,0 +1,56 @@
+//
+//  ThingsToDoList.swift
+//  Academia
+//
+//  Created by Kelly Johnson on 10/3/18.
+//  Copyright © 2018 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+
+
+// THINGS TO DO
+
+    // Models
+        // Add UID String properties to all relevant Models
+
+    // Model Controllers
+        // User Model Controllers
+            // Adult Student Model Controller
+                // CRUD Functions
+            // Kid Student Model Controller
+                // CRUD Functions
+            // Owner Model Controller
+                // CRUD Functions
+        // Location Model Controller
+            // CRUD Functions
+        // Adult Basic Belt Model Controller
+            // CRUD Functions
+        // Adult Black Belt Model Controller
+            // CRUD Functions
+        // Kids Belt Model Controller
+            // CRUD Functions
+        // Aula Model Controller
+            // CRUD Functions
+        // Payemnt Program Model Controller
+            // CRUD Functions
+        // Group Model Controller
+            // CRUD Functions
+        // OnBoarding Task Model Controller
+            // CRUD Functions
+
+    // Core Data
+        // create models
+        // refactor existing models
+        // refactor existing model controllers
+
+    // Picker Wheels
+        // Owner New Payment Program
+        // Owner Add class
+        // Belt Creation?
+
+    // Add new Students to group Button fucntionality
+        // collection view add
+
+
+
