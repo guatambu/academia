4e84aeb3c30d6bbf9e12eda3cd3eaf00e2ed339d
michael guatambu davis <guatambu@dundak.com>
2018-12-01 19:35:03 -0800

    adds location VCS.swift files and wires them up to corresponding OwnerProfileFlow storyboard VCs

---
+++ Academia/Controllers/View Controllers/Owner Role/Profile Menu VCs/Locations/LocationContactInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationContactInfoViewController.swift
+//  Academia
+//
+//  Created by Michael Guatambu Davis on 12/1/18.
+//  Copyright © 2018 DunDak, LLC. All rights reserved.
+//
+
+import UIKit
+
+class LocationContactInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Academia/Controllers/View Controllers/Owner Role/Profile Menu VCs/Locations/LocationInfoDetailsViewController.swift
+++ Academia/Controllers/View Controllers/Owner Role/Profile Menu VCs/Locations/LocationInfoDetailsViewController.swift
@@ -2,7 +2,7 @@
 //  LocationInfoDetailsViewController.swift
 //  Academia
 //
-//  Created by Kelly Johnson on 11/30/18.
+//  Created by Michael Guatambu Davis on 11/30/18.
 //  Copyright © 2018 DunDak, LLC. All rights reserved.
 //
 
