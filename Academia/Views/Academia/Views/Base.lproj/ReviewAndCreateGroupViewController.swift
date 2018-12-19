dbfbb84454d935f2820fe9186df93dc6ac913971
michael guatambu davis <guatambu@dundak.com>
2018-12-18 23:00:59 -0800

    adds group creation workflow to storyboards and basic wireups to corresponding swift files

---
+++ Academia/Views/Academia/Views/Base.lproj/ReviewAndCreateGroupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReviewAndCreateGroupViewController.swift
+//  Academia
+//
+//  Created by Kelly Johnson on 12/18/18.
+//  Copyright © 2018 DunDak, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ReviewAndCreateGroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
