528e8a868d8d8470663f93093b782fdf832bf36b
michael guatambu davis <guatambu@dundak.com>
2019-01-04 12:16:06 -0800

    adds ability to view individual student profile in Group creation workflow in AddStudentsToGroupTVC

--- Academia/Views/Academia/Views/Base.lproj/AddStudentsToGroupTableViewController.swift
+++ Academia/Views/Academia/Views/Base.lproj/AddStudentsToGroupTableViewController.swift
@@ -51,7 +51,7 @@ class AddStudentsToGroupTableViewController: UITableViewController, GroupMembers
         super.viewDidLoad()
         
         // set up delegate relationship with AddNewStudentGroupTableViewCell
-        tableView.delegate = self
+        //tableView.delegate = self
 
     }
     
@@ -81,7 +81,6 @@ class AddStudentsToGroupTableViewController: UITableViewController, GroupMembers
         
         // programmatically performing the segue
         
-        print("to billing details segue")
         // instantiate the relevant storyboard
         let mainView: UIStoryboard = UIStoryboard(name: "OwnerStudentsFlow", bundle: nil)
         // instantiate the desired TableViewController as ViewController on relevant storyboard
@@ -90,13 +89,13 @@ class AddStudentsToGroupTableViewController: UITableViewController, GroupMembers
         // run check to see is there are groupMembers
         
         guard let kidMembers = kidMembers, let adultMembers = adultMembers else {
-            print("ERROR: fail to unwrap kidMembers and/or adultmembers. AddStudentsToGroupTableViewController.swift -> saveButtontapped() - line 93")
+            print("ERROR: fail to unwrap kidMembers and/or adultmembers. AddStudentsToGroupTableViewController.swift -> nextButtontapped() - line 92")
             return
         }
         
         guard kidMembers.isEmpty == true && adultMembers.isEmpty == true else {
             
-            welcomeInstructionsLabelOutlet.textColor = UIColor.red
+            welcomeInstructionsLabelOutlet.textColor = beltBuilder.redBeltRed
             return
         }
         
@@ -153,15 +152,91 @@ class AddStudentsToGroupTableViewController: UITableViewController, GroupMembers
 
         // Configure the cell...
         if indexPath.section == 0 {
-            cell.adultStudent = mockAdults[indexPath.row]
+            cell.kidStudent = mockKids[indexPath.row]
             
         } else if indexPath.section == 1 {
-            cell.kidStudent = mockKids[indexPath.row]
+            cell.adultStudent = mockAdults[indexPath.row]
             
         }
 
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // programmatically performing the segue
+        
+        // instantiate the relevant storyboard
+        let mainView: UIStoryboard = UIStoryboard(name: "OwnerStudentsFlow", bundle: nil)
+        // instantiate the desired TableViewController as ViewController on relevant storyboard
+        let destViewController = mainView.instantiateViewController(withIdentifier: "toProfileComplete") as! OwnersStudentDetailViewController
+        
+        // create the segue programmatically - PUSH
+        self.navigationController?.pushViewController(destViewController, animated: true)
+
+        // set the desired properties of the destinationVC's navgation Item
+        let backButtonItem = UIBarButtonItem()
+        backButtonItem.title = " "
+        navigationItem.backBarButtonItem = backButtonItem
+        
+        if indexPath.section == 0 {
+            // kidStudent setup
+            let kid = mockKids[indexPath.row]
+            
+            destViewController.isOwner = false
+            destViewController.isKid = true
+            destViewController.username = kid.username
+            destViewController.password = kid.password
+            destViewController.firstName = kid.firstName
+            destViewController.lastName = kid.lastName
+            destViewController.parentGuardian = kid.parentGuardian
+            destViewController.profilePic = kid.profilePic
+            destViewController.birthdate = kid.birthdate
+            destViewController.beltLevel = kid.belt.beltLevel
+            destViewController.numberOfStripes = kid.belt.numberOfStripes
+            destViewController.addressLine1 = kid.addressLine1
+            destViewController.addressLine2 = kid.addressLine2
+            destViewController.city = kid.city
+            destViewController.state = kid.state
+            destViewController.zipCode = kid.zipCode
+            destViewController.phone = kid.phone
+            destViewController.mobile = kid.mobile
+            destViewController.email = kid.email
+            destViewController.emergencyContactName = kid.emergencyContactName
+            destViewController.emergencyContactRelationship = kid.emergencyContactRelationship
+            destViewController.emergencyContactPhone = kid.emergencyContactPhone
+            
+        } else if indexPath.section == 1 {
+            // adultStudent setup
+            let adult = mockAdults[indexPath.row]
+            
+            destViewController.isOwner = false
+            destViewController.isKid = false
+            destViewController.username = adult.username
+            destViewController.password = adult.password
+            destViewController.firstName = adult.firstName
+            destViewController.lastName = adult.lastName
+            destViewController.profilePic = adult.profilePic
+            destViewController.birthdate = adult.birthdate
+            destViewController.beltLevel = adult.belt.beltLevel
+            destViewController.numberOfStripes = adult.belt.numberOfStripes
+            destViewController.addressLine1 = adult.addressLine1
+            destViewController.addressLine2 = adult.addressLine2
+            destViewController.city = adult.city
+            destViewController.state = adult.state
+            destViewController.zipCode = adult.zipCode
+            destViewController.phone = adult.phone
+            destViewController.mobile = adult.mobile
+            destViewController.email = adult.email
+            destViewController.emergencyContactName = adult.emergencyContactName
+            destViewController.emergencyContactRelationship = adult.emergencyContactRelationship
+            destViewController.emergencyContactPhone = adult.emergencyContactPhone
+            
+        }
+        
+    }
+    
+    
 
 }
 
