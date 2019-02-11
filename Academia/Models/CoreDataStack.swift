ac1520e65b5202d7afa137ded0b2a9c85cd11db9
michael guatambu davis <guatambu@dundak.com>
2019-02-10 20:05:10 -0800

    adds prject specifications to CoreDataStack and its error handling

--- Academia/Models/CoreDataStack.swift
+++ Academia/Models/CoreDataStack.swift
@@ -14,10 +14,10 @@ class CoreDataStack {
     
     static let container: NSPersistentContainer = {
         
-        let container = NSPersistentContainer(name: "ReplaceWithNameOfYourCoreDataModelFile")
+        let container = NSPersistentContainer(name: "Academia")
         container.loadPersistentStores() { (storeDescription, error) in
             if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+                fatalError("ERROR: Unresolved error \(error), \(error.userInfo).  CoreDataStack.swift -> container - line 20.")
             }
         }
         return container
