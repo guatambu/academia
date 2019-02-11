658cd5ddc3a4d11e796963285a1047aa6ddb144c
michael guatambu davis <guatambu@dundak.com>
2019-02-10 20:01:36 -0800

    adds CoreDataStack.swift and basic file setup and organization for the corresponding CoreData+Cinvenience init files

---
+++ Academia/Models/CoreData+Convenience/AdultStudentCD+Convenience.swift
@@ -0,0 +1,13 @@
+//
+//  AdultStudentCD+Convenience.swift
+//  Academia
+//
+//  Created by Michael Guatambu Davis on 2/10/19.
+//  Copyright © 2019 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+
+extension AdultStudentCD {
+    
+}
---
+++ Academia/Models/CoreData+Convenience/BillingTypeCD+Convenience.swift
@@ -0,0 +1,15 @@
+//
+//  BillingTypeCD+Convenience.swift
+//  Academia
+//
+//  Created by Michael Guatambu Davis on 2/10/19.
+//  Copyright © 2019 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension BillingTypeCD {
+    
+}
