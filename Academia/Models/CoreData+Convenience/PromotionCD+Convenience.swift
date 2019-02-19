0365e82a9944f18efba1a96311cafe8725935a20
michael guatambu davis <guatambu@dundak.com>
2019-02-18 17:58:03 -0800

    adds owner relationship property to location and adds PromotionCD data model to project

---
+++ Academia/Models/CoreData+Convenience/PromotionCD+Convenience.swift
@@ -0,0 +1,26 @@
+//
+//  PromotionCD+Convenience.swift
+//  Academia
+//
+//  Created by Michael Guatambu Davis on 2/18/19.
+//  Copyright © 2019 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension PromotionCD {
+    
+    // convenience initializer to allow creation of an PromotionCD object via Academia CoreDataStack's managedObjectContext
+    convenience init(promotionDate: Date = Date(), // do i want to manually set the date?
+                     numberOfStripes: Int16,
+                     beltLevel: String,
+                     context: NSManagedObjectContext = CoreDataStack.context) {
+        
+        self.init(context: context)
+        
+        self.numberOfStripes = numberOfStripes
+        self.beltLevel = beltLevel
+    }
+    
+}
