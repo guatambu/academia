467304c8db7ffdac367098e1326a123b2fb684f8
michael guatambu davis <guatambu@dundak.com>
2019-02-26 21:52:30 -0800

    adds AddressCD to CoreData and corresponding extension with Convenience initializer

---
+++ Academia/Models/CoreData+Convenience/Address+Convenience.swift
@@ -0,0 +1,31 @@
+//
+//  Address+Convenience.swift
+//  Academia
+//
+//  Created by Michael Guatambu Davis on 2/26/19.
+//  Copyright © 2019 DunDak, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension AddressCD {
+    
+    // convenience initializer to allow creation of a AddressCD object via Academia CoreDataStack's managedObjectContext
+    convenience init(addressLine1: String,
+                     addressLine2: String,
+                     city: String,
+                     state: String,
+                     zipCode: String,
+                     context: NSManagedObjectContext = CoreDataStack.context) {
+        
+        self.init(context: context)
+        
+        self.addressLine1 = addressLine1
+        self.addressLine2 = addressLine2
+        self.city = city
+        self.state = state
+        self.zipCode = zipCode
+    }
+}
+
